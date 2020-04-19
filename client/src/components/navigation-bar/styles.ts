5d08b8945ce8947bbfd47d37e59b66f69fb891a1
Jan Mathew <janmathew@MacBookPro.local>
2020-04-19 22:38:10 +0800

    fix(navigation bar): adjusted even space between items

--- client/src/components/navigation-bar/styles.ts
+++ client/src/components/navigation-bar/styles.ts
@@ -11,7 +11,7 @@ export const Container = styled.div`
 
 export const Menu = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-evenly;
   width: 500px;
 
   > div,
